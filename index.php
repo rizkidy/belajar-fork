6cdffe2316a2f67c400f06a4b38799c7ca03d4d3
Ivan Nur Efendi <nurpendi180@gmail.com>
2022-10-27 10:57:26 +0700

    belajar php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$namaSaya = "Ivan Nur Efendi";
+$umurSaya = 20;
+$umurAdek = 10;
+$selisihUmur = $umurSaya - $umurAdek;
+
+echo "Belajar PHP Dasar 1 <br>";
+echo "Nama saya <strong>$namaSaya</strong>, saya berusia <strong>$umurSaya</strong> tahun. <br>";
+echo "Selisih umur saya dengan adek saya adalah = $selisihUmur tahun.";
+
+?>
